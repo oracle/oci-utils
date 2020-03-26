dffbc02f20fba2ef8c4030edb089fdeb2e3dfc4a
Emmanuel Jannetti <4819607+ejannett@users.noreply.github.com>
2020-03-26 21:37:29 +0100

    fix some headers

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 
-# Copyright (c) 2017, 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown
+# at http://oss.oracle.com/licenses/upl.
 
 """ Build an rpm from oci-utils.
 """
--- tools/decorators.py
+++ tools/decorators.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 
 # Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown
